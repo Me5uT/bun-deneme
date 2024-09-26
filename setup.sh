0bf3a4ad23bf96b7fb16aff2dcb083e0ce32343d
ilaydazeynepozdemir <izozdemir@mirketsecurity.com>
2024-09-26 20:02:00 +0300

    Dockerfile

---
+++ setup.sh
@@ -0,0 +1,8 @@
+bun run clean
+bun install &
+sleep 3
+bun install
+bunx --bun vite build
+docker build -t bun-deneme-app .
+docker stop fe-bun
+docker run --name fe-bun -p 9000:9000 bun-deneme-app &
--- setup/setup.sh
+++
@@ -1,4 +0,0 @@
-bun install
-bunx --bun vite build
-docker build -t bun-deneme-app .
-docker run -p 9000:9000 bun-deneme-app &
