2a228ec0e28f2fbe7b20a7b3a6f59a78b5d981f8
ilaydazeynepozdemir <izozdemir@mirketsecurity.com>
2024-09-27 11:57:40 +0300

    Dockerfile

--- setup.sh
+++ setup.sh
@@ -1,9 +1,27 @@
 bun run clean
-bun install &
-sleep 3
-bun install
+
+### bun install ###
+TIMEOUT=30
+echo "bun install komutu çalıştırılıyor..."
+timeout $TIMEOUT bun install
+if [ $? -ne 0 ]; then
+    echo "İlk deneme başarısız veya zaman aşımına uğradı, tekrar deneniyor..."
+    timeout $TIMEOUT bun install
+    if [ $? -ne 0 ]; then
+        echo "Komut tekrar başarısız oldu."
+        exit 1
+    else
+        echo "Komut başarıyla tamamlandı."
+    fi
+else
+    echo "Komut başarıyla tamamlandı."
+fi
+### ### ###
+
 bunx --bun vite build
 docker build -t bun-deneme-app .
 docker stop fe-bun
+
 sudo docker container prune --force
-docker run --name fe-bun -p 9000:9000 bun-deneme-app &
+sudo docker run --name fe-bun -p 9000:9000 bun-deneme-app &
+sudo docker image prune --force
