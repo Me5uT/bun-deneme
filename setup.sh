2dbe0628f15a47231cf61b09967f3cf8b0069609
ilaydazeynepozdemir <izozdemir@mirketsecurity.com>
2024-09-27 12:21:05 +0300

    Dockerfile

--- setup.sh
+++ setup.sh
@@ -1,4 +1,16 @@
 
+ENV_FILE=".env"
+
+NEW_PROD_URL="https://dev-admin.mirketsecurity.com"
+#NEW_PROD_URL="https://dev-test.mirketsecurity.com"
+#NEW_PROD_URL="https://admin.mirketsecurity.com"
+# VITE_API_PROD_URL değerini değiştirme
+sed -i "s|^VITE_API_PROD_URL=.*|VITE_API_PROD_URL=$NEW_PROD_URL|" $ENV_FILE
+
+echo ".env dosyasında VITE_API_DEV_URL ve VITE_API_PROD_URL güncellendi."
+
+
+
 bun run clean
 
 ### bun install ###
@@ -27,8 +39,9 @@ fi
 
 bunx --bun vite build
 sudo docker build -t bun-deneme-app .
-sudo docker stop fe-bun
 
+sudo docker stop fe-bun
 sudo docker container prune --force
+
 sudo docker run --name fe-bun -p 9000:9000 bun-deneme-app &
 sudo docker image prune --force
